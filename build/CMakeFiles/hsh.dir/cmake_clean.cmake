c51043a4f1866fc257f7bae46bea53dbde06e757
Sumato Ken <bermime1@gmail.com>
2023-05-13 18:40:49 +0100

    First hello world!

---
+++ build/CMakeFiles/hsh.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hsh.dir/helloWorld.c.o"
+  "CMakeFiles/hsh.dir/helloWorld.c.o.d"
+  "hsh"
+  "hsh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ helloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Hello World!" to the standard output
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Hello World!\n");
+	return (0);
+}
