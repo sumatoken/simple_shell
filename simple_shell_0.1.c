633f9d7621fc9bb63038fc357360d3c92d7313df
Sumato Ken <bermime1@gmail.com>
2023-05-14 13:46:13 +0100

    Outputting error with write

--- simple_shell_0.1.c
+++ simple_shell_0.1.c
@@ -129,7 +129,8 @@ void simple_shell_0_1(void)
 
 		if (stat(command, &st) == -1)
 		{
-			printf("%s: NOT FOUND\n", command);
+			write(STDOUT_FILENO, command, _strlen(command));
+			write(STDOUT_FILENO, ": NOT FOUND\n", 12);
 			free(command);
 			continue;
 		}
