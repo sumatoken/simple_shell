9c9fbeb2ea23cd0c4635ee1c303aaa943dfeb4ff
Sumato Ken <bermime1@gmail.com>
2023-05-24 20:23:09 +0100

    Editing error message

--- simple_shell_0.1.c
+++ simple_shell_0.1.c
@@ -119,7 +119,7 @@ void simple_shell_0_1(char **environ)
 		if (stat(command, &st) == -1)
 		{
 			write(STDOUT_FILENO, command, _strlen(command));
-			write(STDOUT_FILENO, ": NOT FOUND\n", 12);
+			write(STDOUT_FILENO, ": command not found\n", 12);
 			free(command);
 			continue;
 		}
